6f76e632be34d14f08236d589efdcb99e79a4eaf
Vinoth Kumar Mahendira <vinoth23675@gmail.com>
2017-02-18 22:37:31 +0530

    sample

--- JavaByteExample.java
+++ JavaByteExample.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
   byte b1 = 100;
  byte b2 = 20;
  System.out.println("Value of byte variable b1 is :" + b1);
-System.out.println("Value of byte variable b1 is :" + b2);
+System.out.println(" New changes Value of byte variable b1 is :" + b2);
  }
 }
